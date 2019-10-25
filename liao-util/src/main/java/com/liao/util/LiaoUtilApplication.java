600920b30436d5191546041a42a1b40e4c7ece63
liaohongbin <805398283@qq.com>
2019-10-25 14:47:19 +0800

    提交日常工具测试model

---
+++ liao-util/src/main/java/com/liao/util/LiaoUtilApplication.java
@@ -0,0 +1,13 @@
+package com.liao.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaoUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiaoUtilApplication.class, args);
+	}
+
+}
---
+++ liao-util/src/test/java/com/liao/util/LiaoUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liao.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiaoUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
