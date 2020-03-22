66f56af9ace41229ffa731e07f9b714c9a563f6e
liaohongbin <469840290@qq.com>
2020-03-23 00:06:19 +0800

    动态代理实例

---
+++ liao-design/src/main/java/com/liao/design/service/Test.java
@@ -0,0 +1,25 @@
+package com.liao.design.service;
+
+import com.liao.design.proxy.UserProxyInvocationHandler;
+import com.liao.design.service.UserService;
+import com.liao.design.service.UserServiceImpl;
+import org.apache.catalina.User;
+
+/**
+ * @author lhb
+ * @Title: com.liao.design
+ * @ProjectName JavaJvmStudyInfo
+ * @Description: for Test  handle
+ * @Date create in 23:52 2020/3/22
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        UserService userService=new UserServiceImpl();
+        UserProxyInvocationHandler userProxyInvocationHandler=new  UserProxyInvocationHandler();
+        userProxyInvocationHandler.setTarget(userService);
+        UserService proxy = (UserService) userProxyInvocationHandler.getProxy();
+        proxy.addUser();
+    }
+}
