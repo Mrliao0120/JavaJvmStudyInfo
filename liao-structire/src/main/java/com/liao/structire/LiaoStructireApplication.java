64dff8b0f20723d40160c1420d5ed6512b8a0410
liaohongbin <469840290@qq.com>
2020-06-14 16:34:19 +0800

    提交原型模式文档说明

---
+++ liao-structire/src/main/java/com/liao/structire/LiaoStructireApplication.java
@@ -0,0 +1,13 @@
+package com.liao.structire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaoStructireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiaoStructireApplication.class, args);
+	}
+
+}
---
+++ liao-structire/src/test/java/com/liao/structire/LiaoStructireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liao.structire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiaoStructireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
